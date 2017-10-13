c7209d67c2fb1ec7c10869d8b33d6c8eb3681c60
David Brennan <ddbren@gmail.com>
2017-10-13 19:13:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+    kittens.unshift(name)
+    return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  
+}
+
+function destructivelyRemoveLastKitten() {
+  
+}
+
+function destructivelyRemoveFirstKitten() {
+  
+}
+
